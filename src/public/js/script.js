9bf87bb495fb7d211d9bc6e3d02885edf40204b2
Renato Zamudio Castellanos <juniorasmat40@gmail.com>
2020-09-09 21:22:08 -0500

    mostrabdo video en la vista

---
+++ src/public/js/script.js
@@ -0,0 +1,33 @@
+
+/* El método MediaDevices.getUserMedia() solicita al usuario 
+permisos para usar un dispositivo de entrada de vídeo y/o 
+uno de audio como una cámara o compartir la pantalla y/o micrófono.
+Si el usuario proporciona los permisos, entonces le retornará un Promise
+que es resuelto por el resultado del objeto MediaStream. 
+Si el usuario niega el permiso, o si el recurso multimedia no es válido,
+ entonces el promise es rechazado con PermissionDeniedError o 
+ NotFoundError respectivamente. Nótese que es posible que el 
+ promise retornado no sea ni resuelto ni rechazado, 
+ya que no se requiere que el usuario tome una decisión.  */
+const videoGrid=document.getElementById('video-grid');
+console.log(videoGrid);
+const myVideo=document.createElement('video');
+myVideo.muted=true;
+
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
+}).then(stream =>{
+    myVideoStream=stream;
+    addVideoStream(myVideo,stream);
+});
+
+const addVideoStream=(video,stream)=>{
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+    
+}
